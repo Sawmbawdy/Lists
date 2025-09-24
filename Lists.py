7d7f3039715c83f9a55b2a969a008807e293d8a5
Sawmbawdy <saiompatro12@gmail.com>
2025-09-24 19:41:29 -0400

    Add files via upload

---
+++ Lists.py
@@ -0,0 +1,9 @@
+Emptylist = []
+print(Emptylist)
+
+elementalist = [1, 2, 3, 4, 5]
+print(elementalist, "\n")
+stringlist = ['Hi', 'Bye']
+print("Saying Hi and Bye 2 times", stringlist * 2, "\n")
+mixedlist = [1, 'Hi', 2.5, True]
+print("A mixed list reversed", mixedlist[::-1], "\n")
