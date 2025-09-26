c61cb00b75ecdc463ad31795e1d8ab6c32dd87c1
Sawmbawdy <saiompatro12@gmail.com>
2025-09-26 18:09:03 -0400

    Add files via upload

---
+++ PlayingWithLists.py
@@ -0,0 +1,6 @@
+L = [1,2,23,0,32,4,2,23,4,5,6,7,8,9]
+print("The original list is:", L)
+print("The average of the list is:", sum(L)/len(L))
+print("The maximum number in the list is:", max(L))
+print("The minimum number in the list is:", min(L))
+print("The sum of the list is:", sum(L))
